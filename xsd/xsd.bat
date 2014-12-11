1f8d279d4410d3d662d8ab5f882ef05742beaf90
Robert Lowe <robertmlowe@rmlowe.com>
2014-12-11 23:55:01 +0800

    module.cs, records.cs

---
+++ xsd/xsd.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" /classes /namespace:NetDimensions.Apis.LearningPath /outputdir:..\Excellence learningPath.xsd
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" /classes /namespace:NetDimensions.Apis.Module /outputdir:..\Excellence module.xsd
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" /classes /namespace:NetDimensions.Apis.Records /outputdir:..\Excellence records.xsd
