f31d5741bfcc157592f7e19eae7575c0d4b421a5
Robert Lowe <robertmlowe@rmlowe.com>
2015-02-21 20:22:31 +0800

    Adding test project

---
+++ ExcellenceTests/ExcellenceTest.cs
@@ -0,0 +1,23 @@
+﻿using NetDimensions.Apis.LearningPath;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Xml.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExcellenceTests
+{
+    [TestClass]
+    public class ExcellenceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string xml = ExcellenceTests.Properties.Resources.learningPath;
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(xml);
+            Stream stream = new MemoryStream(bytes);
+            learningPath lp = (learningPath) new XmlSerializer(typeof(learningPath)).Deserialize(stream);
+            Assert.AreEqual(3, lp.jobProfile.Length);
+        }
+    }
+}
