8c15fa10b61d960d1f992589d3908a05f366872c
Robert Lowe <robertmlowe@rmlowe.com>
2015-03-08 17:36:12 +0800

    Adding untracked files

---
+++ NetDimensionsApis/xsd/xsd.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" /classes /namespace:NetDimensions.Apis.LearningPath /outputdir:.. learningPath.xsd
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" /classes /namespace:NetDimensions.Apis.Module /outputdir:.. module.xsd
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" /classes /namespace:NetDimensions.Apis.Records /outputdir:.. records.xsd
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" /classes /namespace:NetDimensions.Apis.CompetenciesAwarded /outputdir:.. competenciesAwarded.xsd
